7053874a59b454fd5d19fefb20f58f9f112a802b
FamHaggs <pedrofamh@protonmail.com>
2021-09-19 21:05:23 -0400

    some changes

--- Game.java
+++ Game.java
@@ -14,39 +14,77 @@ public class Game {
 		Country russia = new Country("Russia", random.nextInt(10));
 		Country canada = new Country("Canada", random.nextInt(10));
 		Country england = new Country("England", random.nextInt(10));
+		Country china = new Country("china", random.nextInt(10));
+		Country ukraine = new Country("ukraine", random.nextInt(10));
+		Country argentina = new Country("argentina", random.nextInt(10));
+		Country india = new Country("india", random.nextInt(10));
 		
-		ArrayList<Country> all_countrys = new ArrayList<>(); // Create an ArrayList object
-		// TODO: ArrayList<Country> all_dead_countrys = new ArrayList<>(); 
+		ArrayList<Country> all_countries = new ArrayList<>(); // Create an ArrayList object
+		ArrayList<Country> all_dead_countries = new ArrayList<>(); 
 		
-		all_countrys.add(america);
-		all_countrys.add(germany);
-		all_countrys.add(brazil);
-		all_countrys.add(russia);
-		all_countrys.add(canada);
-		all_countrys.add(england);
-		america.kill(germany);
-		simulate_day(all_countrys);
+		all_countries.add(america);
+		all_countries.add(germany);
+		all_countries.add(brazil);
+		all_countries.add(russia);
+		all_countries.add(canada);
+		all_countries.add(england);
+		all_countries.add(china);
+		all_countries.add(ukraine);
+		all_countries.add(argentina);
+		all_countries.add(india);
+		
+		//all_dead_coutries = update_dead();
+		all_countries = simulate_day(all_countries);
+		//all_countries = update_survivors(all_countries);
+		for(int i=0;i<all_countries.size();i++) {
+			System.out.println(all_countries.get(i).name);
+		}
 		/**
 		System.out.println(germany.is_alive);
-		update_survivors(all_countrys);
-		System.out.println(all_countrys.get(0));
+		update_survivors(all_countries);
+		System.out.println(all_countries.get(0));
 		**/
 	}
-	static void update_survivors(ArrayList<Country> a) {
+	static ArrayList<Country> update_dead(ArrayList<Country> alive_list, ArrayList<Country> dead_list){//not working, dumb design
+		for(int i=0;i < alive_list.size();i++) {
+			if(!alive_list.get(i).is_alive) {
+				dead_list.add(alive_list.get(i));
+			}
+		}
+		return dead_list;
+	}
+	
+	static ArrayList<Country> update_survivors(ArrayList<Country> a) {
 		for(int i=0;i < a.size();i++) {
 			if(!a.get(i).is_alive) {
-				System.out.println(a.get(i).name);
+				//System.out.println(a.get(i).name);
 				a.remove(i);
 			}
 		}
+		return a;
 	}
-	static void simulate_day(ArrayList<Country> a) {
+	static ArrayList<Country> simulate_day(ArrayList<Country> a) {
 		// 3 random strength, 2 kills
 		Random r = new Random();
-		int bar = r.nextInt(4);
-		a.get(5).change_strength(bar);
+		// 3 random changes to strength
+		for(int i=0;i<3;i++) {
+			int rand_country = r.nextInt(a.size());
+			int rand_msg = r.nextInt(4);
+			//System.out.println(rand_country +"  " +rand_msg);
+			a.get(rand_country).change_strength(rand_msg);
+		}
+		for(int j=0;j<2;j++) {
+			int rand_country1 = r.nextInt(a.size());
+			int rand_country2 = r.nextInt(a.size());
+			if(a.get(rand_country1).strength > a.get(rand_country2).strength) { // if country_a.strength > country_b.strength
+				a.get(rand_country1).kill(a.get(rand_country2)); //country_a. kills country_b
+			} else {
+				a.get(rand_country2).kill(a.get(rand_country1)); // country_b. kills country_a
+			}
+		}
+		a = update_survivors(a);
+		return a;
 		
-		//for(int i=0;i<3;)
 	}
 	
 
