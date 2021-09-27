d979442f75fc6369365b00feae75d1423887d0ff
FamHaggs <pedrofamh@protonmail.com>
2021-09-27 09:51:31 -0300

    Add files via upload

--- Country.java
+++ Country.java
@@ -6,25 +6,27 @@ public class Country {
 	public boolean is_alive;
 	public String name;
 	public Team team; 
+	public String image_path;
 	
 	public int strength;
 	public int daysAlive;
-	public int kills;
+	public int kill_counter;
 	
-	public Country(String name, int strength) {
+	public Country(String name, int strength, String image_path) {
 		this.name = name;
 		this.strength = strength;
 		
 		this.is_alive = true;
 		this.daysAlive = 0;
-		this.kills = 0;
+		this.kill_counter = 0;
+		this.image_path = image_path;
 		
 	}
 	
 	public void kill(Country p) {
 		if(this.team == null || this.team != p.team) {
 			System.out.println(this.name + " Killed: " + p.name);
-			this.kills++;
+			this.kill_counter++;
 			p.is_alive = false;				
 		}
 	}
@@ -33,25 +35,25 @@ public class Country {
 		this.team = Team.values()[rand_int];
 	}
 	public void change_strength(int random_num) {
-		String msg1 = "General has been killed, -1 point of strength";
-		String msg2 = "Tanks with engine problems, -1 point of strength";
-		String msg3 = ", +1 point of strength";
-		String msg4 = "Scientits developed a new weapon, +1 point of strength";
+		String msg1 = " :General has been killed, -1 point of strength: ";
+		String msg2 = " :Tanks with engine problems, -1 point of strength: ";
+		String msg3 = " :+1 point of strength: ";
+		String msg4 = " :Scientits developed a new weapon, +1 point of strength: ";
 		switch (random_num) {
 		case 0:
-			System.out.println(msg1);
+			System.out.println(this.name + msg1 + this.strength);
 			this.strength--;
 			break;
 		case 1:
-			System.out.println(msg2);
+			System.out.println(this.name + msg2 + this.strength);
 			this.strength--;
 			break;
 		case 2:
-			System.out.println(msg3);
+			System.out.println(this.name + msg3 + this.strength);
 			this.strength++;
 			break;
 		case 3:
-			System.out.println(msg3);
+			System.out.println(this.name + msg4 + this.strength);
 			this.strength++;
 			break;
 		}
