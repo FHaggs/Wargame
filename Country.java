46a8db601bf3b5f788739a48e5175348f3de9313
FamHaggs <pedrofamh@protonmail.com>
2021-10-07 10:35:25 -0300

    Add files via upload

--- Country.java
+++ Country.java
@@ -21,39 +21,44 @@ public class Country {
 		
 	}
 	
-	public void kill(Country p) {
-		if(this.team == null || this.team != p.team && this.name != p.name) {
-			System.out.println(this.name + " Killed: " + p.name);
-			this.kill_counter++;
-			p.is_alive = false;				
+	public boolean kill(Country p) {
+		if(this.team == null || this.team != p.team) {
+			if(this.name != p.name) {
+				System.out.println(this.name + " Killed: " + p.name);
+				this.kill_counter++;
+				p.is_alive = false;
+				return true;				
+			}
 		}
+		return false;
 	}
 	public void join_team(Team rand_team) {
 			this.team = rand_team;
 	}
-	public void change_strength(int random_num) {
+	public String change_strength(int random_num) {
 		String msg1 = " :General has been killed, -1 point of strength: ";
 		String msg2 = " :Tanks with engine problems, -1 point of strength: ";
-		String msg3 = " :+1 point of strength: ";
+		String msg3 = " :More soldiers arrived, +1 point of strength: ";
 		String msg4 = " :Scientits developed a new weapon, +1 point of strength: ";
 		switch (random_num) {
 		case 0:
 			System.out.println(this.name + msg1 + this.strength);
 			this.strength--;
-			break;
+			return msg1;
 		case 1:
 			System.out.println(this.name + msg2 + this.strength);
 			this.strength--;
-			break;
+			return msg2;
 		case 2:
 			System.out.println(this.name + msg3 + this.strength);
 			this.strength++;
-			break;
-		case 3:
+			return msg3;
+		default:
 			System.out.println(this.name + msg4 + this.strength);
 			this.strength++;
-			break;
+			return msg4;
 		}
+
 		
 	}
 }
--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+
 import java.awt.color.ColorSpace;
 //import java.awt.color.ColorSpace;
 import java.util.Random;
@@ -7,14 +8,9 @@ import javax.imageio.ImageIO;
 
 import java.util.ArrayList;
 
-//import java.nio.file.*;
+
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorConvertOp;
-//import java.awt.image.ColorConvertOp;
-//import java.io.*;
-//import javax.imageio.ImageIO;
-//import java.awt.*;
-//import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -23,21 +19,25 @@ public class Game {
 	static int day=1;
 	static ArrayList<Country> dead_countries = new ArrayList<>();
 	
+	static ArrayList<Country> killer_and_killed = new ArrayList<>();
+	
+	static ArrayList<String> msgs_day = new ArrayList<>();
+	
 	public static void main(String[] args)  throws IOException{
 		Random random = new Random(); 
 		
 		
-		Country america = new Country("America", random.nextInt(10), "/home/pedro/eclipse-workspace/HungerGames/flags/america.jpg");
-		Country germany = new Country("Germany", random.nextInt(10), "/home/pedro/eclipse-workspace/HungerGames/flags/germany.png");
-		Country brazil = new Country("Brazil", random.nextInt(10), "/home/pedro/eclipse-workspace/HungerGames/flags/brasil.png");
-		Country russia = new Country("Russia", random.nextInt(10), "/home/pedro/eclipse-workspace/HungerGames/flags/russia.png");
-		Country canada = new Country("Canada", random.nextInt(10), "/home/pedro/eclipse-workspace/HungerGames/flags/canada.png");
-		Country england = new Country("England", random.nextInt(10), "/home/pedro/eclipse-workspace/HungerGames/flags/england.png");
-		Country china = new Country("china", random.nextInt(10), "/home/pedro/eclipse-workspace/HungerGames/flags/china.png");
-		Country ukraine = new Country("ukraine", random.nextInt(10), "/home/pedro/eclipse-workspace/HungerGames/flags/ukraine.jpg");
-		Country argentina = new Country("argentina", random.nextInt(10), "/home/pedro/eclipse-workspace/HungerGames/flags/argentina.jpg");
-		Country india = new Country("india", random.nextInt(10), "/home/pedro/eclipse-workspace/HungerGames/flags/india.png");
-		Country chile = new Country("chile", random.nextInt(10), "/home/pedro/eclipse-workspace/HungerGames/flags/chile.jpg");
+		Country america = new Country("America", random.nextInt(10), "flags/america.jpg");
+		Country germany = new Country("Germany", random.nextInt(10), "flags/germany.png");
+		Country brazil = new Country("Brazil", random.nextInt(10), "flags/brasil.png");
+		Country russia = new Country("Russia", random.nextInt(10), "flags/russia.png");
+		Country canada = new Country("Canada", random.nextInt(10), "flags/canada.png");
+		Country england = new Country("England", random.nextInt(10), "flags/england.png");
+		Country china = new Country("china", random.nextInt(10), "flags/china.png");
+		Country ukraine = new Country("ukraine", random.nextInt(10), "flags/ukraine.jpg");
+		Country argentina = new Country("argentina", random.nextInt(10), "flags/argentina.jpg");
+		Country india = new Country("india", random.nextInt(10), "flags/india.png");
+		Country chile = new Country("chile", random.nextInt(10), "flags/chile.jpg");
 		
 		ArrayList<Country> all_countries = new ArrayList<>(); // Create an ArrayList object
 
@@ -54,12 +54,13 @@ public class Game {
 		all_countries.add(india);
 		all_countries.add(chile);
 		
-		
+		File folder = new File("days");
+		delete_folder(folder);
 		
 		while(all_countries.size()>1) {
 		
 			try {
-				save_image(all_countries, dead_countries);			
+				save_image(all_countries, dead_countries, killer_and_killed, msgs_day);			
 			}catch(Exception e) {
 				System.out.println(e);
 			}
@@ -70,7 +71,11 @@ public class Game {
 			System.out.println("\n");
 		
 		}
-		//save_image(all_countries.get(0),day, true);
+		try {
+			save_image(all_countries, dead_countries, killer_and_killed, msgs_day);			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 		System.out.println("Winner: " + all_countries.get(0).name + " Kills:" + all_countries.get(0).kill_counter);
 	}
 
@@ -93,10 +98,9 @@ public class Game {
 		// 3 random changes to strength
 		for(int i=0;i<3;i++) {
 			int rand_country = r.nextInt(a.size());
-			int rand_msg = r.nextInt(4);
-			//System.out.println(rand_country +"  " +rand_msg);
-			a.get(rand_country).change_strength(rand_msg);
-			
+			int rand_msg = r.nextInt(3);
+			String msg = a.get(rand_country).change_strength(rand_msg);
+			msgs_day.add(a.get(rand_country).name+" "+msg + a.get(rand_country).strength);			
 		}
 		for(int i=0;i<2;i++) {
 			int rand_country = r.nextInt(a.size());
@@ -111,38 +115,70 @@ public class Game {
 		for(int j=0;j<2;j++) {
 			int rand_country1 = r.nextInt(a.size());
 			int rand_country2 = r.nextInt(a.size());
-			if(a.get(rand_country1).strength > a.get(rand_country2).strength) { // if country_a.strength > country_b.strength
-				a.get(rand_country1).kill(a.get(rand_country2)); //country_a. kills country_b
-				a = update_survivors(a);
+			if(a.get(rand_country1).strength > a.get(rand_country2).strength) { // if country_a.strength > country_b.strength     && a.get(rand_country1) != a.get(rand_country2)
+				if(a.get(rand_country1).kill(a.get(rand_country2))) {
+					killer_and_killed.add(a.get(rand_country1));
+					killer_and_killed.add(a.get(rand_country2));					
+					a = update_survivors(a);
+				} //country_a. kills country_b
 			} else {
-				a.get(rand_country2).kill(a.get(rand_country1)); // country_b. kills country_a
-				a = update_survivors(a);
+				if(a.get(rand_country2).kill(a.get(rand_country1))) { // country_b. kills country_a
+					killer_and_killed.add(a.get(rand_country2));					
+					killer_and_killed.add(a.get(rand_country1));
+					a = update_survivors(a);
+				} 
 			}
 		}
 
 		
-		//a = update_survivors(a);
 		return a;
 		
 	}
-	static void save_image(ArrayList<Country> a, ArrayList<Country> d) throws IOException {
-		BufferedImage newimg = new BufferedImage(1100,1100,BufferedImage.TYPE_INT_ARGB);
+	static void save_image(ArrayList<Country> a, ArrayList<Country> d, ArrayList<Country> k, ArrayList<String> msgs) throws IOException {
+		BufferedImage newimg = new BufferedImage(1150,1300,BufferedImage.TYPE_INT_ARGB);
 		Graphics2D graphics =newimg.createGraphics();
 		int x = 0;
-		int y = 0;
+		int y = 200;
+		
+		//int msg_x = 0;
+		int msg_y = 0;
+		
 		int row_counter = 1;
 		int col_counter = 0;
+		graphics.setFont(new Font("Arial Black", Font.BOLD, 30));
+		
+		for(int i=0;i< msgs.size();i++) {
+			graphics.drawString(msgs.get(i), 210, 40+msg_y);
+			msg_y = msg_y + 30;
+		}
+		
+		if(k.size()>0) {			
+			graphics.drawString("Killer                     Kiled", 210, 190);
+		}
+		for(int i=0; i<k.size();i++) {
+			
+			Image image = javax.imageio.ImageIO.read(new File(k.get(i).image_path));
+			image = image.getScaledInstance(150, 100, Image.SCALE_SMOOTH);
+			
+
+			graphics.drawImage(image,x+210*row_counter,y+col_counter,null);
+			
+			if(row_counter >= 2) {
+				row_counter = 0;
+				col_counter = col_counter + 100;
+			}
+			row_counter += 1;
+			
+		}
 		for(int i=0;i<a.size();i++) {
 			
 			Image image = javax.imageio.ImageIO.read(new File(a.get(i).image_path));
 			image = image.getScaledInstance(150, 100, Image.SCALE_SMOOTH);
 			
-			//System.out.println(x+160*row_counter);
-			//System.out.println(y+col_counter);
+
 			graphics.drawImage(image,x+160*row_counter,y+col_counter,null);
 			
 			if(row_counter >= 3) {
-				//System.out.println("GKGGHGHKJGHKJ");
 				row_counter = 0;
 				col_counter = col_counter + 100;
 			}
@@ -161,16 +197,13 @@ public class Game {
 		        BufferedImage image1 = op.filter(buf_image, null);
 
 
-		        //image = buf_image;
 				graphics.drawImage(image1,x+160*row_counter,y+col_counter,null);
 				if(row_counter >= 3) {
-					//System.out.println("GKGGHGHKJGHKJ");
 					row_counter = 0;
 					col_counter = col_counter + 100;
 				}
 				row_counter++;
 				
-				//ImageIO.write(result, "png", output);
 				
 				
 				
@@ -179,10 +212,12 @@ public class Game {
 				e.printStackTrace();
 			}
 		}
-		ImageIO.write(newimg,"png",new File("day_"+String.valueOf(day)+".png"));
+		ImageIO.write(newimg,"png",new File("days/day_"+String.valueOf(day)+".png"));
+		killer_and_killed.clear();
+		msgs_day.clear();
 
 	}
-	public static BufferedImage toBufferedImage(Image img)
+	static BufferedImage toBufferedImage(Image img)
 	{
 	    if (img instanceof BufferedImage)
 	    {
@@ -199,6 +234,14 @@ public class Game {
 
 	    // Return the buffered image
 	    return bimage;
+	}
+	static void delete_folder(File index) {
+		String[]entries = index.list();
+		for(String s: entries){
+		    File currentFile = new File(index.getPath(),s);
+		    currentFile.delete();
+		    //System.out.println(currentFile.toString());
+		}
 	}
 	
 }
